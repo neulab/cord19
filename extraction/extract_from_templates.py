023f6361f45c8675cbf8300ab216db8ddd6f4b2e
Graham Neubig <neubig@gmail.com>
2020-03-20 18:18:52 -0400

    Updated to consider OIE extractions, and also tracebacks

--- extraction/extract_from_templates.py
+++ extraction/extract_from_templates.py
@@ -7,18 +7,17 @@ from collections import defaultdict
 
 virus_names = ['COVID-19', 'Wuhan coronavirus', 'Wuhan seafood market pneumonia virus', 'SARS2', 'coronavirus disease 2019', 'SARS-CoV-2', '2019-nCoV']
 
+oie_span_re = r'#[0-9]+,[0-9]+'
+
 if __name__ == "__main__":
 
   parser = argparse.ArgumentParser(description='Extract data from text/OIE extractions')
   parser.add_argument('--template_file', required=True, type=str, default='cord19-templates.csv', help='input file')
-  parser.add_argument('--text_files', required=True, type=str, nargs='+', help='Text files')
+  parser.add_argument('--text_files', type=str, nargs='+', help='Text files')
   parser.add_argument('--oie_files', type=str, nargs='+', help='OpenIE extractions')
   parser.add_argument('--tasks', type=int, nargs='+', default=None, help='Which tasks to do (if not specified, all)')
 
   args = parser.parse_args()
-
-  if args.oie_files and len(args.text_files != args.oie_files):
-    raise ValueError('length of text_files and oie_files arguments must be identical')
 
   with open(args.template_file, 'r') as f:
     csvf = csv.reader(f)
@@ -27,9 +26,11 @@ if __name__ == "__main__":
     if args.tasks is not None:
       temp_data = [temp_data[i] for i in args.tasks]
 
-  temp_regexes = []
+  # Create regexes
+  temp_regexes, oie_regexes = [], []
   v_regex = '('+'|'.join(virus_names)+')'
   for i, my_data in enumerate(temp_data):
+    # Text extraction
     orig_regexes = my_data[4].split('\n')
     regexes = []
     for x in orig_regexes:
@@ -39,36 +40,71 @@ if __name__ == "__main__":
         regexes.append(x)
     if not len(regexes):
       temp_regexes.append(None)
-      continue
-    exp_regexes = []
-    gname = 0
-    for y in regexes:
-      exp_regexes.append(y.replace('[X]', v_regex).replace('[Y]', f'(?P<G{gname}>.*?)'))
-      gname += 1
-    exp_regexes = '('+'|'.join(exp_regexes)+')'
-    temp_regexes.append( (re.compile(exp_regexes), gname) )
-  temp_recounts = [defaultdict(lambda: 0) for _ in temp_regexes]
+    else:
+      regexes = [x.replace('[X]', v_regex).replace('[Y]', f'(?P<G{i}>.*?)') for (i,x) in enumerate(regexes)]
+      regexes = '('+'|'.join(regexes)+')'
+      temp_regexes.append( (re.compile(regexes), len(regexes), my_data[6]) )
+    # OIE regexes
+    regexes = my_data[3].split('\n')
+    if not len(regexes) or not len(regexes[0]):
+      oie_regexes.append(None)
+    else:
+      regexes = [x.replace('[X]', v_regex).replace('[B]', '\\|\\|\\|') for (i,x) in enumerate(regexes)]
+      regexes = '('+'|'.join(regexes)+')'
+      oie_regexes.append( re.compile(regexes) )
+  temp_recounts = [defaultdict(lambda: []) for _ in temp_regexes]
+  oie_recounts = [defaultdict(lambda: []) for _ in oie_regexes]
 
+  # Process text
   lines = []
-  for fname in args.text_files:
+  for file_id, fname in enumerate(args.text_files if args.text_files else []):
     print(f'Processing {fname}', file=sys.stderr)
     with open(fname, 'r') as f:
-      for line in tqdm.tqdm(f):
-        for temp_rex, temp_rec in zip(temp_regexes, temp_recounts):
+      for line_id, line in tqdm.tqdm(enumerate(f)):
+        for temp_id, (temp_rex, temp_rec) in enumerate(zip(temp_regexes, temp_recounts)):
           if temp_rex:
-            temp_rex_re, temp_rex_cnt = temp_rex
+            temp_rex_re, temp_rex_cnt, temp_rex_type = temp_rex
             m = re.search(temp_rex_re, line)
             if m:
-              vals = [m.group(f'G{i}') for i in range(temp_rex_cnt)]
-              vals = [x for x in vals if x is not None]
-              assert(len(vals) == 1)
-              temp_rec[vals[0]] += 1
+              if temp_rex_type == 'ydata':
+                vals = [m.group(f'G{i}') for i in range(temp_rex_cnt)]
+                vals = [x for x in vals if x is not None]
+                assert(len(vals) == 1)
+                key = vals[0]
+              else:
+                key = m.group(1)
+              temp_rec[key].append( (file_id,line_id) )
 
-  for temp_d, temp_rex, temp_rec in zip(temp_data, temp_regexes, temp_recounts):
+  # Process oie
+  lines = []
+  for file_id, fname in enumerate(args.oie_files if args.oie_files else []):
+    print(f'Processing {fname}', file=sys.stderr)
+    with open(fname, 'r') as f:
+      for line_id, line in tqdm.tqdm(enumerate(f)):
+        line = re.sub(oie_span_re,'',line)
+        for extraction in line.split('\t'):
+          for temp_id, (oie_rex, oie_rec) in enumerate(zip(oie_regexes, oie_recounts)):
+            if oie_rex:
+              m = re.search(oie_rex, extraction)
+              if m:
+                key = extraction.strip().replace('|||', ' ')
+                oie_rec[key].append( (file_id,line_id) )
+
+  for temp_d, temp_rex, temp_rec, oie_rex, oie_rec in zip(temp_data, temp_regexes, temp_recounts, oie_regexes, oie_recounts):
     if temp_rex:
       print(f'------- {temp_d[1]} regex results')
-      res = sorted(list(temp_rec.items()), key=lambda x: -x[1])
+      res = sorted(list(temp_rec.items()), key=lambda x: -len(x))
       for k, v in res:
-        print(f'{v}\t{k} {temp_d[5]}')
+        l = len(v)
+        files = ' '.join([f'{args.text_files[fid]}:{lid}' for (fid,lid) in v])
+        print(f'{l}\t{k} {temp_d[5]}\t{files}')
+      print()
+    if oie_rex:
+      print(f'------- {temp_d[1]} regex results')
+      res = sorted(list(oie_rec.items()), key=lambda x: -len(x))
+      for k, v in res:
+        l = len(v)
+        files = ' '.join([f'{args.oie_files[fid]}:{lid}' for (fid,lid) in v])
+        print(f'{l}\t{k} {temp_d[5]}\t{files}')
       print()
 
